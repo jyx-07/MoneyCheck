fce6a6643448b2f4356337c144f97523d4e611ae
jyx-07 <s25069@gsm.hs.kr>
2026-06-04 15:23:29 +0900

    test(budgets): 테스트 모듈 의존성 모킹 추가

--- src/budgets/budgets.controller.spec.ts
+++ src/budgets/budgets.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BudgetsController } from './budgets.controller';
+import { BudgetsService } from './budgets.service';
 
 describe('BudgetsController', () => {
   let controller: BudgetsController;
@@ -7,6 +8,19 @@ describe('BudgetsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BudgetsController],
+      providers: [
+        {
+          provide: BudgetsService,
+          useValue: {
+            findAll: jest.fn(),
+            findByYearAndMonth: jest.fn(),
+            findOne: jest.fn(),
+            create: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<BudgetsController>(BudgetsController);
--- src/budgets/budgets.service.spec.ts
+++ src/budgets/budgets.service.spec.ts
@@ -1,12 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BudgetsService } from './budgets.service';
+import { EntityManager } from '@mikro-orm/postgresql';
+import { getRepositoryToken } from '@mikro-orm/nestjs';
+import { Budget } from './entity/budget.entity';
+import { Category } from '../categories/entity/category.entity';
 
 describe('BudgetsService', () => {
   let service: BudgetsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BudgetsService],
+      providers: [
+        BudgetsService,
+        {
+          provide: EntityManager,
+          useValue: {
+            create: jest.fn(),
+            flush: jest.fn(),
+            assign: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+        {
+          provide: getRepositoryToken(Budget),
+          useValue: {
+            findAll: jest.fn(),
+            findByYearAndMonth: jest.fn(),
+            findByIdOrFail: jest.fn(),
+            findByYearMonthAndCategory: jest.fn(),
+          },
+        },
+        {
+          provide: getRepositoryToken(Category),
+          useValue: {
+            findByIdOrFail: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<BudgetsService>(BudgetsService);
